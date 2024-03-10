c0395bfbf1a78bae77c93cf11c1c7839fd039f90
Thinh Nguyen <thinh.nguyen@dirox.net>
2024-03-10 16:52:28 +0700

    initialize app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './assets/scss/app.scss';
+import reactLogo from '/assets/images/react.svg';
+import viteLogo from '/assets/images/vite.svg';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
